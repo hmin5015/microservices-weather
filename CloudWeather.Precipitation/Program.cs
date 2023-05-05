ae9b9af766b89e35507ffe3be2dcd2600bb9f010
스터디하자 <hmin5015@gmail.com>
2023-05-05 01:08:08 -0700

    add data access and migrate to postgreSQL

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,12 +1,29 @@
+using CloudWeather.Precipitation.DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<PrecipitationDbContext>(opts =>
+{
+    opts.EnableSensitiveDataLogging();
+    opts.EnableDetailedErrors();
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDbConnection"));
+}, ServiceLifetime.Transient);
+
 var app = builder.Build();
 
-app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) =>
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, PrecipitationDbContext db) =>
 {
-    return Results.Ok(zip);
+    if (days == null || days < 1 || days > 30)
+        return Results.BadRequest("Please provide a 'days' query parameter between 1 and 30");
+
+    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = await db.Precipitation
+        .Where(precip => precip.ZipCode == zip && precip.CreatedOn > startDate)
+        .ToListAsync();
+
+    return Results.Ok(results);
 });
 
 app.Run();
